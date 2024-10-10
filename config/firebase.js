dca312a79242df78b43660251cf09a68106f843a
042Vidhi <98405549+042Vidhi@users.noreply.github.com>
2024-10-11 00:31:55 +0530

    fix

--- config/firebase.js
+++ config/firebase.js
@@ -7,15 +7,24 @@
 
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    // const firebaseConfig = {
+    //     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    //     authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    //     projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    //     storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    //     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    //     appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    //     measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+    //   };
     const firebaseConfig = {
-        apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
-      };
+      apiKey: "AIzaSyB7j240fVelPWdlQ5axXIhFF9-KBi-odiQ",
+      authDomain: "campus-foodz.firebaseapp.com",
+      projectId: "campus-foodz",
+      storageBucket: "campus-foodz.appspot.com",
+      messagingSenderId: "403397435299",
+      appId: "1:403397435299:web:6b63ce04d6cddddf58c416",
+      measurementId: "G-NF2NMTHH58"
+    };
 
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
